2cb890eae884a90322ea7da58220ab090ff920ac
Mukhtar Janagale <janogale@users.noreply.github.com>
2020-09-06 14:34:32 +0300

    Updates

---
+++ precache-manifest.f799c9f2109d721459ea5827a721c870.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33b7140f8b59ae3f3848c26e2cfd7fe",
+    "url": "/alumni_new/index.html"
+  },
+  {
+    "revision": "640938f331facee3e949",
+    "url": "/alumni_new/static/css/main.d358ec6c.chunk.css"
+  },
+  {
+    "revision": "7b75ffb5b41c4b081ced",
+    "url": "/alumni_new/static/js/2.56ff0696.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/alumni_new/static/js/2.56ff0696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640938f331facee3e949",
+    "url": "/alumni_new/static/js/main.dbf810e6.chunk.js"
+  },
+  {
+    "revision": "37fcc30ded98edd4e0c1",
+    "url": "/alumni_new/static/js/runtime-main.ea797859.js"
+  },
+  {
+    "revision": "3741c04b3c380ae0067b9602eb3de59a",
+    "url": "/alumni_new/static/media/building.3741c04b.jpg"
+  }
+]);
