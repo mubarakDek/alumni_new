a09b7491509bac2f7530dd22599a66fdd34ffa60
Mukhtar Janagale <janogale@users.noreply.github.com>
2020-09-08 16:39:32 +0300

    Added filtering members by year

--- src/pages/members/Members.js
+++ src/pages/members/Members.js
@@ -3,6 +3,8 @@ import "../members/membersStyle.scss";
 import Member from "../../components/member/Member";
 import SectionTitle from "../../components/SectionTitle";
 import FilterSelect from "../../components/filterSelect/FilterSelect";
+import Loader from "../../components/loader";
+import axios from "axios";
 
 // api url
 import { apiURL } from "../../globals";
@@ -11,8 +13,25 @@ import { apiURL } from "../../globals";
 import useSWR from "swr";
 
 function Members() {
-  const { data, error } = useSWR(`${apiURL}/items/member`);
+  const [data, setData] = React.useState({});
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+
+  // const { data, error, mutate } = useSWR(`${apiURL}/items/member`);
   const { data: detail } = useSWR(`${apiURL}/items/memberdetail`);
+
+  React.useEffect(function () {
+    axios
+      .get(`${apiURL}/items/member`)
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+      });
+  }, []);
 
   if (error) {
     return (
@@ -27,13 +46,35 @@ function Members() {
     );
   }
 
+  function filterData(value) {
+    setLoading(true);
+    axios
+      .get(`${apiURL}/items/member`)
+      .then((res) => {
+        let newData = res.data.data.filter((user) => user.year.includes(value));
+
+        setData({ data: newData });
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+      });
+  }
+
+  if (loading) {
+    return <Loader></Loader>;
+  }
+
   if (data && detail) {
     const { data: member } = data;
     const { data: memberdetail } = detail;
     const [...mem] = memberdetail;
     const [m] = mem;
 
-    console.log(m);
+    if (!member) {
+      return <Loader></Loader>;
+    }
 
     return (
       <section className="members">
@@ -44,11 +85,36 @@ function Members() {
             <div className="members_content_main_left">
               <h1 className="px-3 py-2">Filters</h1>
               <div className="filters">
-                <FilterSelect title="Class" />
-                <FilterSelect title="Faculty" />
-                <FilterSelect title="Degree" />
-                <FilterSelect title="Location" />
-                <FilterSelect title="Other Education" />
+                <FilterSelect
+                  filterData={filterData}
+                  title="Class"
+                  options={[
+                    2009,
+                    2010,
+                    2011,
+                    2012,
+                    2013,
+                    2014,
+                    2015,
+                    2016,
+                    2017,
+                    2018,
+                    2019,
+                    2020,
+                  ]}
+                />
+                <FilterSelect
+                  title="Faculty"
+                  options={["ICT", "Engineering", "Accounting"]}
+                  filterData={filterData}
+                />
+                <FilterSelect
+                  filterData={filterData}
+                  title="Degree"
+                  options={["Bachelor", "Masters"]}
+                />
+                <FilterSelect filterData={filterData} title="Location" />
+                <FilterSelect filterData={filterData} title="Other Education" />
               </div>
             </div>
             <div className="members_content_main_right px-4 pt-4">
